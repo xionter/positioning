cd2e2d01aeda948d16b9ac0bcec92d59c062a3a7
i_mikhailov <i_mikhailov@tochka.com>
2026-04-07 17:55:09 +0500

    push

--- index.js
+++ index.js
@@ -5,3 +5,11 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+// Задание 2: Модальное окно
+const openBtn = document.querySelector('#openModal');
+const closeBtn = document.querySelector('#closeModal');
+const overlay = document.querySelector('#modalOverlay');
+
+openBtn.addEventListener('click', () => overlay.classList.remove('hidden'));
+closeBtn.addEventListener('click', () => overlay.classList.add('hidden'));
